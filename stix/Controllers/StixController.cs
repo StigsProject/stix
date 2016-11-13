664b0c9d80d9c18afefd2a68a651b3efbd28c0d9
chrisnain <chrisnain@hotmail.com>
2016-11-13 21:19:29 +0100

    Added simple Stig Controller.

---
+++ stix/Controllers/StixController.cs
@@ -0,0 +1,24 @@
+﻿namespace stix.Controllers
+{
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Mvc;
+	using stixAPI.Models;
+	using stixAPI.Data;
+	using Microsoft.EntityFrameworkCore;
+
+	public class StixController : Controller
+	{
+		private readonly StixDbContext context;
+
+		public StixController(StixDbContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return this.View(await this.context.Stig.ToListAsync());
+		}
+	}
+}
